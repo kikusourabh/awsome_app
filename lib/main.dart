d00140ebb0a4e4d2dc1a2b6d512e0e577f172332
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-25 22:20:39 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -16,35 +16,32 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awsome App"),
       ),
-      body: Container(
-        color: Colors.teal,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.red,
-              alignment: Alignment.center,
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.yellow,
-              alignment: Alignment.center,
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.green,
-              alignment: Alignment.center,
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            width: 100,
+            height: 100,
+            color: Colors.red,
+            alignment: Alignment.center,
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            width: 100,
+            height: 100,
+            color: Colors.yellow,
+            alignment: Alignment.center,
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            width: 100,
+            height: 100,
+            color: Colors.green,
+            alignment: Alignment.center,
+          ),
+        ],
       ),
     );
   }
